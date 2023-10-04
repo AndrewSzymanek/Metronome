5a1efadb7fc0fd9df0d4406b956749fe438d7c41
AndrewSzymanek <andrew.szymanek@comcast.net>
2023-10-04 16:59:51 -0500

    added functionality for increasing/decreasing tempo and beats per measure

---
+++ app.js
@@ -0,0 +1,50 @@
+const tempoDisplay = document.querySelector(".tempo");
+const tempoText = document.querySelector(".tempo-text");
+const decreaseTempoBtn = document.querySelector(".decrease-tempo");
+const increaseTempoBtn = document.querySelector(".increase-tempo");
+const tempoSlider = document.querySelector(".slider");
+const startStopBtn = document.querySelector(".start-stop");
+const subtractBeats = document.querySelector(".subtract-beats");
+const addBeats = document.querySelector(".add-beats");
+const measureCount = document.querySelector(".measure-count");
+
+let bpm = 140;
+let beatsPerMeasure = 4;
+
+decreaseTempoBtn.addEventListener("click", () => {
+  if (bpm <= 20) {
+    return;
+  }
+  bpm--;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+});
+increaseTempoBtn.addEventListener("click", () => {
+  if (bpm >= 280) {
+    return;
+  }
+  bpm++;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+});
+
+tempoSlider.addEventListener("input", () => {
+  bpm = tempoSlider.value;
+  tempoDisplay.textContent = bpm;
+  tempoSlider.value = bpm;
+});
+
+subtractBeats.addEventListener("click", () => {
+  if (beatsPerMeasure <= 1) {
+    return;
+  }
+  beatsPerMeasure--;
+  measureCount.textContent = beatsPerMeasure;
+});
+addBeats.addEventListener("click", () => {
+  if (beatsPerMeasure >= 6) {
+    return;
+  }
+  beatsPerMeasure++;
+  measureCount.textContent = beatsPerMeasure;
+});
